4e2d39d190a0d9cf248141c3b057820f378c0c2f
Harper Foley <harper.e.foley@gmail.com>
2022-11-21 22:30:21 +0000

    Adds Book schema to server.js

--- server.js
+++ server.js
@@ -4,10 +4,28 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+//use mongoose
+const mongoose = require('mongoose');
+const { Book } = mongoose;
+
+const bookSchema = new Book ({
+  title: String,
+  description: String,
+  status: String
+});
+
+// add validation to confirm we are wired up to our mongo DB
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Mongoose is connected');
+});
+
+
 const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.get('/test', (request, response) => {
 
