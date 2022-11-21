d3fe10a79fb1b68d40cd38431f6e9a9f9078991f
Harper Foley <harper.e.foley@gmail.com>
2022-11-21 22:51:53 +0000

    Fixes merge conflicts

--- Book.js
+++ Book.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require('mongoose');
 
 const { Book } = mongoose;
@@ -10,15 +9,3 @@ const bookSchema = new Schema ({
 })
 
 const  = mongoose.model('', )
-
-=======
-// book module
-// define schema
-
-// Example schema
-// Book = {
-//   title: null,
-//   description: null ,
-//   status: null
-// };
->>>>>>> edba259481530de99b22dffef78da5da97b8db1c
